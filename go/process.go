acf06f8e5b3218c302e24918c37240d3d7527119
Ruben Schade <git@rubenschade.com>
2015-05-12 16:56:44 +1000

    created go folder, for future sed optimisation

---
+++ go/process.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "os"
+)
+
+func main {
+
+content, err := ioutil.ReadFile("test")
+if error != nil {
+
+}
+lines := strings.Split(string(content), "\n")
+
+}
