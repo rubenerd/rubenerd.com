d49f87822f12208bc2efeee34047552cef429990
Ruben Schade <git@rubenschade.com>
2015-06-14 11:14:12 +1000

    refactored

--- scripts/new-episode.sh
+++ scripts/new-episode.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 ######
-## Script to generate new podcast episode assets
+## Script to generate new podcast episode file
+## Uses previous episode as template
+## It should use a Hugo archetype; and we should also exercise more 
+## Created 2015-06-12
 
 set -e
 
 _archive="content/post/show/"
-_date_fmt="%Y-%m-%dT%H:%M:%S%z"
+_time_fmt="%H:%M:%S%z"
+_date_fmt="%Y-%m-%d"
+_editor="mvim"
 
 ## Get latest episode file and number
 _previous_episode=`ls -1 ${_archive} | tail -n 1`
@@ -18,14 +23,25 @@ _new_episode="show${_new_number}.html"
 cp -f ${_archive}${_previous_episode} ${_archive}${_new_episode}
 
 ## Get current date with Hugo formatting
+_time=`date +"${_time_fmt}"`
 _date=`date +"${_date_fmt}"`
 
-## Update copied file with new values
-sed -i '' -e 's/${_previous_number}/${_new_number}/' ${_archive}${_new_episode}
-
-## Replace date
-sed -i '' -e "s/^date.*/date: \"${_date}\"/" ${_archive}${_new_episode}
-
-## Replace episode numbers
+## Replace episode number
 sed -i '' -e "s/${_previous_number}/${_new_number}/g" \
     ${_archive}${_new_episode}
+
+## Replace date
+sed -i '' -e "s/^date.*/date: \"${_date}T${_time}\"/" \
+    ${_archive}${_new_episode}
+
+## Replace title
+sed -i '' -e "s/^title.*/title: \"Rubénerd ${_new_number} ${_date}\"/" \
+    ${_archive}${_new_episode}
+
+echo "New episode ${_new_number} created, opening editor..."
+`${_editor} ${_archive}${_new_episode}` &
+
+exit
+
+## EOF
+
