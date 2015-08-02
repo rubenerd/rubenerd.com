fa9fb044d8c35b74e548a5dc45fcf2a55527a9e7
Ruben Schade <git@rubenschade.com>
2015-08-02 11:09:46 +1000

    fixed error message

--- scripts/encode-episode.sh
+++ scripts/encode-episode.sh
@@ -2,11 +2,21 @@
 
 ######
 ## Encodes file with lame
+## In future, will allow to convert from MP4/AAC and such
+##
+## 2015-07-27: Created
 
+set -e
+
+## Check dependencies
 command -v lame >/dev/null 2>&1 || {
-    echo "eyeD3 not installed, or not in \$PATH.";
+    echo "lame not installed. Try running brew install lame.";
     exit 1
 }
 
+## Encode!
 lame -m j -q 0 --vbr-new -b 128 --verbose $1
+
+## EOF
+exit 0
 
