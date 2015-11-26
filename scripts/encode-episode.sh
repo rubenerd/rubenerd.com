49d62075c0b68f63f0f446da67e1c83700a1d414
Ruben Schade <git@rubenschade.com>
2015-11-26 14:21:30 +1100

    updated to encode to mp3 from caf

--- scripts/encode-episode.sh
+++ scripts/encode-episode.sh
@@ -1,52 +1,46 @@
 #!/bin/sh
 
 ######
-## Encodes file with lame
-## In future, will allow to convert from MP4/AAC and such
+## Podcast CAF->WAV->MP3 encoder
+## I prefer AAC, but there are stragglers
 ##
+## 2015-11-26: Cleaned up, use CAF as input format now
 ## 2015-08-20: Converts MP4/AAC
 ## 2015-07-27: Created
 
 set -e
+set -x
 
-function exists() {
-    if command -v $1 > /dev/null 2>&1; then
-        return 0
-    else
-        echo "$1 not installed, terminating."
-        return 1
-    fi
-}
-
-    _wav=`echo $1 | sed 's/m4a//'`
-    ffmpeg -i '$1' -ac 2 '$_wav'
-
-function wav_to_mp3() {
-    lame -m j -q 0 --vbr-new -b 128 --verbose $1
+exists() {
+    for tool in $1; do
+        if command -v $1 > /dev/null 2>&1; then
+            return 0
+        else
+            echo "$1 not installed, terminating."
+            return 1
+        fi
+    done
 }
 
 ## Check dependencies
-exists 'lame' && exists 'normalize' && exists 'ffmpeg' || exit 1
+exists "ffmpeg flac lame normalize" || exit 1
 
+## Get name and strip extension
+_original=$1
+_id=`printf "%s" "$_original" | sed 's/\.caf$//g'`
 
+## Convert to WAV
+ffmpeg -i "$_original" "$_id.wav"
 
-## Convert m4a to wav
-m4a_to_wav '$1'
+## Encode to MP3
+lame -m j -q 0 --vbr-new -b 128 --verbose "$_id.wav"
 
-normalize '$1'
+## Encode to FLAC for archiving
+flac --delete-input-file --preserve-modtime --verify --best "$_id.wav"
 
-## Convert wav to mp3
-wav_to_mp3 '$1'
+## If we have what we need, delete original
+[ -f "$_id.flac" ] && [ -f "$_id.mp3" ]
 
-
-echo $1
-
-exit
-
-
-## Encode!
-lame -m j -q 0 --vbr-new -b 128 --verbose $1
-
-## EOF
+## That's a wrap
 exit 0
 
