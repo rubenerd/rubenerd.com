d4baebfb3cb8b34c7ab7e830ff4a21307385d441
Ruben Schade <git@rubenschade.com>
2015-12-19 09:37:01 +1100

    script to create ram disk for rendering

---
+++ scripts/ram-disk.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+######
+## An Xmas gift for my poor old SSD: Creates a RAM disk before Hugo previews
+## Ruben Schade 2015-12 <3
+##
+
+set -e
+set -x
+
+_disk_size=256
+
+ram_disk() {
+    _disk_size=`expr $_disk_size \* 2048`
+    diskutil eraseVolume HFS+ 'Hugo' `hdiutil attach -nomount ram://${_disk_size}`
+    rm -rf ../public
+    ln -s /Volumes/Hugo/ public
+}
+
+unmount_ram_disk() {
+    diskutil unmount -f /Volumes/Hugo
+    rm -rf public
+}
+
