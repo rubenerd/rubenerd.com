b7053a42a297ee77dfd851960b85e443dca05455
Ruben Schade <git@rubenschade.com>
2018-06-10 11:47:24 +1000

    Added image fuzzing

--- scripts/retina.sh
+++ scripts/retina.sh
@@ -2,22 +2,25 @@
 
 ######
 ## Generates Retina and quarter size images for blogs
-## 2017-01
+## 2018-01
 
-_RETINA_WIDTH=1000
-_SMALL_WIDTH=`expr $_RETINA_WIDTH / 2`
-_COMMENT="From Rubenerd, Ruben's weblog of nerdish interests"
+_FUZZ=5
+_TARGET_2X=1000
+_COMMENT="From https://rubenerd.com/, Ruben's weblog of nerdish interests"
 _FOLDER="https://rubenerd.com/files/`date +%Y`"
+_SCP="rubenerd.com:`date +%Y`/"
 
 set -e
+set -x
 
 
 ## Check arguments and dependencies #######################################
+
 if [ $# != 1 ]; then
     printf "%s\n" "Usage (and abusage): ./retina.sh [image]"
     exit 1
-elif ! `command -v gm &> /dev/null`; then 
-    printf "%s\n" "GraphicsMagick not found, exiting"
+elif ! `command -v convert &> /dev/null`; then 
+    printf "%s\n" "ImageMagick not found, exiting"
     exit 1
 else
     _SRC=$1;
@@ -25,63 +28,111 @@ fi
 
 
 ## Create names ###########################################################
-_FILE=`gm identify -format %t $_SRC`
-_EXT=`gm identify -format %e $_SRC`
-_RETINA_IMG="$_FILE@2x.$_EXT"
-_SMALL_IMG="$_FILE@1x.$_EXT"
+
+_FILE=`identify -ping -format %t $_SRC`
+_EXT=`identify -ping -format %e $_SRC`
+_2X_IMG="$_FILE@2x.$_EXT"
+_1X_IMG="$_FILE@1x.$_EXT"
 
 
-## Get image attributes ###################################################
-_WIDTH=`gm identify -format %w $_SRC`
-_HEIGHT=`gm identify -format %h $_SRC`
-_QUALITY='98'
-#_QUALITY=`gm identify -format %Q $_SRC`
+## Crop white space #######################################################
+
+_QUALITY=`identify -ping -format "%Q" $_SRC`
+
+convert               \
+    -fuzz "${_FUZZ}%" \
+    -trim             \
+    +repage           \
+    "$_SRC"           \
+    -quality          \
+    "$_QUALITY"       \
+    "$_2X_IMG"
+
+
+## Get cropped image attributes ###########################################
+
+_2X_WIDTH=`identify -ping -format %w $_2X_IMG`
+_2X_HEIGHT=`identify -ping -format %h $_2X_IMG`
 
 
 ## Shrink image if too wide for retina ####################################
-if [ $_WIDTH -gt $_RETINA_WIDTH ]; then
-    _WIDTH=$_RETINA_WIDTH
 
-    gm convert \
-        "$_SRC" \
-        -comment "$_COMMENT" \
-        -resize ${_WIDTH}x! \
-        -quality $_QUALITY \
-        "$_RETINA_IMG"
-else
-    cp "$_SRC" "$_RETINA_IMG" 
-    cp "$_SRC" "$_SMALL_IMG" 
+if [ $_2X_WIDTH -gt $_TARGET_2X ]; then
+    _2X_WIDTH=$_TARGET_2X
+
+    mogrify                   \
+        -resize ${_2X_WIDTH}!x \
+        -comment "$_COMMENT"  \
+        -quality $_QUALITY    \
+        "$_2X_IMG"            
+
+    	_2X_HEIGHT=`identify -ping -format %h $_2X_IMG`
+        echo "Fucking new height: $_2X_HEIGHT"
 fi
 
+
 ## Crop if not /2 (Retina has to be double) ###############################
-_HEIGHT=`gm identify -format %h $_RETINA_IMG`
 
-if expr $_HEIGHT % 2 &> /dev/null; then
-    _HEIGHT=`expr $_HEIGHT - 1`
+_NEEDS_CROPPING="NO"
 
-    gm mogrify \
-        -crop ${_RETINA_WIDTH}x${_HEIGHT}! \
-        "$_RETINA_IMG" 
+#if expr $_2X_HEIGHT % 2 &> /dev/null; then
+if ! `expr $_2X_HEIGHT % 2`; then
+    _2X_HEIGHT=`expr $_2X_HEIGHT - 1`
+    _NEEDS_CROPPING="YES"
+fi
+
+if ! `expr $_2X_WIDTH % 2`; then
+    _2X_WIDTH=`expr $_2X_WIDTH - 1`
+    _NEEDS_CROPPING="YES"
+fi
+
+if [ $_NEEDS_CROPPING == "YES" ]; then
+    mogrify                                    \
+        -crop ${_2X_WIDTH}!x${_2X_HEIGHT}!+0+0 \
+	+repage                                    \
+        "$_2X_IMG" 
 fi
 
 
 ## Create 1x image ########################################################
-_SMALL_WIDTH=`expr $_WIDTH / 2`
-_SMALL_HEIGHT=`expr $_HEIGHT / 2`
 
-gm convert \
-    "$_SRC" \
-    -comment "$_COMMENT" \
-    -resize "$_SMALL_WIDTH" \
-    -quality "$_QUALITY" \
-    "$_SMALL_IMG"
+_1X_WIDTH=`expr $_2X_WIDTH / 2`
+_1X_HEIGHT=`expr $_2X_HEIGHT / 2`
 
 
-## GENERATE HTML ##########################################################
+convert                                  \
+    "$_2X_IMG"                           \
+    -comment "$_COMMENT"                 \
+    -resize ${_1X_WIDTH}!x${_1X_HEIGHT}! \
+    -quality "97"                        \
+    "$_1X_IMG"
 
-_HTML="<p><img src=\"$_FOLDER/$_SMALL_IMG\" alt=\"\" style=\"width:${_SMALL_WIDTH}px; height:${_SMALL_HEIGHT}px\" srcset=\"$_FOLDER/$_SMALL_IMG 1x, $_FOLDER/$_RETINA_IMG 2x\" /></p>"
 
-echo $_HTML
+## Optimise images ########################################################
+
+case "$_EXT" in
+    jpg)
+        jpegoptim --all-progressive "$_2X_IMG" "$_1X_IMG"
+	;;
+    png)
+        pngcrush -brute -reduce -ow -v "$_2X_IMG"
+        pngcrush -brute -reduce -ow -v "$_1X_IMG"
+	;;
+    gif)
+	# TODO: Handle GIFs!
+esac
+
+
+## Generate HTML ##########################################################
+
+_HTML="<p><img src=\"$_FOLDER/$_1X_IMG\" alt=\"\" style=\"width:${_1X_WIDTH}px; height:${_1X_HEIGHT}px\" srcset=\"$_FOLDER/$_1X_IMG 1x, $_FOLDER/$_2X_IMG 2x\" /></p>"
+
+
+## Upload! ################################################################
+
+scp "$_2X_IMG" "$_1X_IMG" "$_SCP"
+
+echo $_HTML | pbcopy
 
 
 ## EOF ##
