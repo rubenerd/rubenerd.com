1aa1cba4869d96c13cbeacffd5bcc81b5cabef54
Ruben Schade <git@rubenschade.com>
2019-04-04 13:29:48 +1100

    Do not set as draft by default, and set title

--- new.sh
+++ new.sh
@@ -2,19 +2,23 @@
 
 ######
 ## New blog post creator, with pre-populated frontmatter
-## I should use gohugo's archetypes instead
+## I should use gohugo's archetypes instead, or a jinja2 template
 
 _PERMALINK=$1
 _DATE=`date "+%FT%H:%M:%S%:z"`
 _YEAR=`date "+%Y"`
 _FOLDER="./content/post/$_YEAR"
 
+## Create content year folder if doesn't exist
 [ ! -d "$_FOLDER" ] && mkdir "$_FOLDER"
 
+## Take a stab at writing the title
+_TITLE=$( echo $_PERMALINK | tr '-' ' ' )
+
+## Template for new post
 tee -a "$_FOLDER/$_PERMALINK.md" << EOF
 ---
-draft: true
-title: ""
+title: "$_TITLE"
 date: "$_DATE"
 abstract: ""
 thumb: ""
